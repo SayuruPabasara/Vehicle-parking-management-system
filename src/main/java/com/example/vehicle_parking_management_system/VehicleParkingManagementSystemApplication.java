746b356c876b83525382822292c449cb1d17a0bd
Thesvgmlwr <themanonly3@gmail.com>
2026-03-09 10:19:31 +0530

    change file structure

---
+++ src/main/java/com/example/vehicle_parking_management_system/VehicleParkingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_parking_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleParkingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleParkingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vehicle_parking_management_system/VehicleParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_parking_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleParkingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
